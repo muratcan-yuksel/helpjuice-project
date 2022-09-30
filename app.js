b8a029c3da76b56c732b22ca7cdb622f36dbeb64
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-10-01 02:46:01 +0300

    add preliminary keydown and modal

---
+++ app.js
@@ -0,0 +1,29 @@
+let inputField = document.querySelector(".inputField");
+
+//keydown function that reads the slash key
+function keydownFunction(event) {
+  let x = event.key;
+
+  if (x == "/") {
+    console.log("You pressed the '/' key!");
+  }
+}
+
+inputField.addEventListener("keydown", keydownFunction);
+//modal
+const modals = document.querySelectorAll("[data-modal]");
+
+modals.forEach(function (trigger) {
+  trigger.addEventListener("click", function (event) {
+    event.preventDefault();
+    const modal = document.getElementById(trigger.dataset.modal);
+    modal.classList.add("open");
+    const exits = modal.querySelectorAll(".modal-exit");
+    exits.forEach(function (exit) {
+      exit.addEventListener("click", function (event) {
+        event.preventDefault();
+        modal.classList.remove("open");
+      });
+    });
+  });
+});
