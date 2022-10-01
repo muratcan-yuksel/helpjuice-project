7b5e032810fd81f78379724b96feaedbd7325025
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-10-01 04:34:49 +0300

    fallback

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ function keydownFunction(event) {
 
   if (x == "/") {
     console.log("You pressed the '/' key!");
+    // modal_container.classList.add("show");
   }
 }
 
@@ -14,11 +15,26 @@ inputField.addEventListener("keydown", keydownFunction);
 const open = document.getElementById("open");
 const modal_container = document.getElementById("modal_container");
 const close = document.getElementById("close");
+const main = document.getElementById("main");
 
 open.addEventListener("click", () => {
   modal_container.classList.add("show");
 });
 
+// if (modal_container.classList.contains("show")) {
 close.addEventListener("click", () => {
   modal_container.classList.remove("show");
+});
+// }
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    modal_container.classList.remove("show");
+  }
+});
+//close modal if clicked outside of modal
+document.body.addEventListener("click", (e) => {
+  console.log("body");
+  if (e.target != open) {
+    modal_container.classList.remove("show");
+  }
 });
