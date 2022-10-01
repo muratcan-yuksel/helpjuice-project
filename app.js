12b08dde58f48ca5f7e4db7f4014947370535972
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-10-01 05:34:06 +0300

    delete show button

--- app.js
+++ app.js
@@ -1,36 +1,30 @@
+// const close = document.getElementById("close");
+// close.addEventListener("click", () => {
+//   modal_container.classList.remove("show");
+// });
 let inputField = document.querySelector(".inputField");
+const modal_container = document.getElementById("modal_container");
+// const main = document.getElementById("main");
 
-//keydown function that reads the slash key
+//keydown function that reads the slash key and opens the modal
 function keydownFunction(event) {
   let x = event.key;
 
   if (x == "/") {
     console.log("You pressed the '/' key!");
-    // modal_container.classList.add("show");
+    modal_container.classList.add("show");
   }
 }
-
+//call the modal opening keydown function
 inputField.addEventListener("keydown", keydownFunction);
-//modal
-const open = document.getElementById("open");
-const modal_container = document.getElementById("modal_container");
-const close = document.getElementById("close");
-const main = document.getElementById("main");
 
-open.addEventListener("click", () => {
-  modal_container.classList.add("show");
-});
-
-// if (modal_container.classList.contains("show")) {
-close.addEventListener("click", () => {
-  modal_container.classList.remove("show");
-});
-// }
+//close modal if escape key is clicked
 document.addEventListener("keydown", (e) => {
   if (e.key === "Escape") {
     modal_container.classList.remove("show");
   }
 });
+
 //close modal if clicked outside of modal
 document.body.addEventListener("click", (e) => {
   console.log("body");
